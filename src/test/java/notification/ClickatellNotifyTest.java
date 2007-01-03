c973763fc65fa8407e6e83d889fc4b08692585f7
Mike Rosseel <mike.rosseel@gmail.com>
2007-01-03 00:50:24 +0000

    move to new server, sync to latest status. Checkin when sms, mailing stuff was finished and tested. Should be a fairly stable baseline

---
+++ src/test/java/notification/ClickatellNotifyTest.java
@@ -0,0 +1,25 @@
+package notification;
+
+import junit.framework.TestCase;
+import media.image.CloudImageImpl;
+
+public class ClickatellNotifyTest extends TestCase {
+
+    public ClickatellNotifyTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public ClickatellNotifyTest(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+
+    public void testMailing() {
+        ClickatellNotify notify = new ClickatellNotify();
+        
+        // Let's avoid automatic testing...
+        // notify.notify(null);
+    }
+
+}
