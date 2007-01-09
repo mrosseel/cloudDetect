a25dbad8924e6168a278346f1a31cfaedb8148d4
Mike Rosseel <mike.rosseel@gmail.com>
2007-01-09 22:00:15 +0000

    still making it headless compatible

---
+++ src/main/java/notification/DummyNotify.java
@@ -0,0 +1,11 @@
+package notification;
+
+import media.image.CloudImage;
+
+public class DummyNotify implements Notifier {
+
+    public void notify(CloudImage image) {
+        //  let's not do anything
+    }
+
+}
