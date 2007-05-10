d5e8e0a543d910274e34e73a5bd0c57341f041ea
Mike Rosseel <mike.rosseel@gmail.com>
2007-05-10 21:34:21 +0000

    new top level pom

--- clouddetect-main/src/main/java/persistence/model/Result.java
+++ clouddetect-main/src/main/java/persistence/model/Result.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity()
 public class Result {
