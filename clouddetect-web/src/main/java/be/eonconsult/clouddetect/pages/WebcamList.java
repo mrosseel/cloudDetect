632626caa39bfbe58f1c70ae9bf0a5f9b23029ea
Mike Rosseel <mike.rosseel@gmail.com>
2007-05-14 22:14:44 +0000

    going to the dao's

--- clouddetect-web/src/main/java/be/eonconsult/clouddetect/pages/WebcamList.java
+++ clouddetect-web/src/main/java/be/eonconsult/clouddetect/pages/WebcamList.java
@@ -1,20 +1,42 @@
 package be.eonconsult.clouddetect.pages;
 
+import java.util.List;
+
+import org.apache.commons.logging.Log;
 import org.apache.tapestry.MarkupWriter;
 import org.apache.tapestry.Renderable;
-import org.apache.tapestry.annotations.Parameter;
+import org.apache.tapestry.annotations.Inject;
+import org.apache.tapestry.spring.SpringBean;
+
+import persistence.dao.FeedDao;
+import persistence.model.Feed;
+
+import com.thoughtworks.xstream.XStream;
 
 public class WebcamList {
+    @Inject
+    @SpringBean("feedDao")
+    private FeedDao feedDao;
+    
+    @Inject
+    private Log log;
+    
     
     private String method;
+    
+    public String getWebcamList() {
+        List<Feed> list = feedDao.getAllActiveFeeds();
+        XStream xstream = new XStream();
+        return xstream.toXML(list)clone();
+    }
     
     public Renderable getFirstBox() {
         return new Renderable()
         {
             public void render(MarkupWriter writer)
             {
-                writer.write("This proves it works.");
-                writer.write(method);
+                log.info("writing webcamlist");
+                writer.write(getWebcamList());
                 
             }
         };
